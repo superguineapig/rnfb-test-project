dea041abd00626bd4ef79f66038305077e36f890
Michael Jarecki <4808737+superguineapig@users.noreply.github.com>
2018-03-29 13:14:03 -0500

    added rnfirebase core

---
+++ ios/Pods/Target Support Files/Pods-RNFBTestProject/Pods-RNFBTestProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNFBTestProject : NSObject
+@end
+@implementation PodsDummy_Pods_RNFBTestProject
+@end
--- ios/RNFBTestProject/AppDelegate.m
+++ ios/RNFBTestProject/AppDelegate.m
@@ -12,6 +12,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Firebase.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -19,6 +21,8 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  
+  [FIRApp configure];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"RNFBTestProject"
